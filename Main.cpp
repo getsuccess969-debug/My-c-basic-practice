c92d21ed69a2059dc46b2907df146f59be329949
getsuccess969-debug <getsuccess969@gmail.com>
2026-06-18 11:30:55 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Integer.h"
+#include <memory>
 Integer operator+(const Integer& lhs, const Integer& rhs) {
 	return Integer(*lhs.ptr + *rhs.ptr);
 }
@@ -11,9 +12,20 @@ Integer operator+(const Integer& lhs, int rhs) {
 Integer operator+(int lhs, const Integer& rhs) {
 	return Integer(lhs + *rhs.ptr);
 }
+std::ostream &operator<<(std::ostream& out, Integer& x) {
+	out << x.get();
+	return out;
+}
+std::istream& operator>>(std::istream &in, Integer& a) {
+	int x;
+	in >> x;
+	a.set(x);
+	return in;
+}
 int main() {
 	Integer i(3), j(4);
-	auto k = j+8;
-	Integer l(j + k+8);
-	std::cout << l.get();
+	auto k = i + j + 10;
+	k();
+	k(0);
+
 }
